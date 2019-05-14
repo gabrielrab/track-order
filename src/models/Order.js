84f348550801286135f8720104588506d64119cb
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-05-14 10:54:30 -0300

    Config package.json
    Configure package.json for server on the deploy

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = mongoose.Schema({
-    code: String,
+    code: {
+        type: String,
+        unique: true
+    },
     remetente:{
         rem_nome: String,
         rem_cep: Number
     },
     sendIn: Date,
     arrivedAt: Date,
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
 });
 
 mongoose.model('Order', OrderSchema);
