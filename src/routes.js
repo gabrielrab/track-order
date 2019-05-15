c02a11d4374bb56d398b5d94c5ddef6388c3266c
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-05-15 17:00:19 -0300

    Client CRUD implement
    - The first page was implemented
    - The CRUD client was created
    - Added some css styles

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 //Import Controllers
 const OrderController = require('./controllers/OrderController');
+const ClientController = require('./controllers/ClientController');
 
 routes.get('/', (req, res)=>{
     res.render("index");     
@@ -14,6 +15,12 @@ routes.get('/order/:orderCode', OrderController.show);
 routes.post('/order', OrderController.create);
 routes.put('/order/:orderCode', OrderController.update);
 routes.delete('/order/:orderCode', OrderController.destroy);
+
+//Client
+routes.get('/client', ClientController.index);
+routes.get('/client/:clientId', ClientController.show);
+routes.post('/client', ClientController.create);
+routes.put('/client/:clientId', ClientController.update);
 
 
 module.exports = routes;
