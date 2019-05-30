4547fc921d8fdccd4f9c17a8d4c1e0ce12c545da
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-05-30 20:31:59 -0300

    Problem on view

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -11,11 +11,10 @@ module.exports = {
     async createOrder(req, res){
         const id = decoded.decodedToken(req, res);
 
-        const clients = await Client.findOne({'clientFor': id}, (err, clients)=>{
+        const clients = await Client.find({'clientFor': id}, (err, clients)=>{
             if(err){
                 return res.status(400).send({error: 'Esse id de usuário não está disponivel. FAvor entrar em contato com o desenvolvedor do sistema.'});
             } else{
-                console.log(clients);
                 return res.render('createOrder', {id: id, r_client: clients});
             }
         });
